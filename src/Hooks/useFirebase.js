94dd963d608e35d70db27614eccf488cb4864e1d
Md Asaduzaman <asadasad9282@gmail.com>
2022-04-09 00:23:08 +0600

    add google sing in using custome hool

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,15 +1,30 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "../fairbase.init";
 
+
+const auth = getAuth(app)
+
+const googleProvider = new GoogleAuthProvider()
 
 const useFirebase = () => {
+
     const [user, setUser] = useState({})
 
-    useEffect(() => {
 
-    })
     const signInWithGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(res => {
+                const user = res.user
+                console.log(user);
+            })
+            .catch(err => console.log(err))
+
+
         console.log('sign in successful');
     }
-    return [user, setUser]
+
+
+    return { user, signInWithGoogle }
 }
 export default useFirebase
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../Hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase()
     return (
         <div>
             <h2>pleass Log in</h2>
+            <button onClick={signInWithGoogle} style={{ margin: '20px' }}>sign in to goolge</button>
             <form>
 
                 <input type="email" placeholder='Your email' />
