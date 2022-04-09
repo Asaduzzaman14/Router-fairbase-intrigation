4ece9b4a5d1a61e5aa7164b0ee5f5174bac11f77
Md Asaduzaman <asadasad9282@gmail.com>
2022-04-09 22:20:58 +0600

    end

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,32 @@
 import { getAuth } from 'firebase/auth';
 import React from 'react';
-// import useFirebase from '../../Hooks/useFirebase';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
 import app from '../../fairbase.init';
 
 const auth = getAuth(app)
 const Login = () => {
 
-    // const { signInWithGoogle } = useFirebase()
     const [signInWithGoogle] = useSignInWithGoogle(auth)
-    //   ************
+
+    const location = useLocation()
+    const navigate = useNavigate()
+    const from = location?.state?.from?.pathname || '/';
+
+    const handelGogleSignIn = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true })
+            })
+    }
 
     return (
+
+
+
         <div>
             <h2>pleass Log in</h2>
-            <button onClick={() => signInWithGoogle()} style={{ margin: '20px' }}>sign in to goolge</button>
+            <button onClick={handelGogleSignIn} style={{ margin: '20px' }}>sign in to goolge</button>
             <form>
 
                 <input type="email" placeholder='Your email' />
